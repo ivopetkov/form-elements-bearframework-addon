5950444d19dbc2a1436f3f7fe8f146d445795c7f
Ivo Petkov <ivo@ivopetkov.com>
2021-08-08 22:55:17 +0300

    Add support for onClick attribute for the submit-button element.

--- components/submit-button.php
+++ components/submit-button.php
@@ -28,6 +28,11 @@ if (isset($attributes['waitingclass'])) {
 
 $enableAfterSubmit = $component->enableAfterSubmit !== 'false';
 
+$onClick = (string) $component->onClick;
+if (strlen($onClick) === 0) {
+    $onClick = 'ivoPetkov.bearFrameworkAddons.formElementsSubmitButton.onClick(this,' . json_encode($waitingText) . ',' . json_encode($waitingClass) . ',' . (int) $enableAfterSubmit . ');';
+}
+
 $attributes['data-form-element-component'] = 'button';
 
 echo '<html><head>';
@@ -40,7 +45,6 @@ $js = include __DIR__ . '/submit-button.min.js.php';
 echo '<script>' . $js . '</script>';
 echo '</head><body>';
 echo '<div ' . Utilities::getContainerAttributes('submit-button', $attributes) . '>';
-$onClick = 'ivoPetkov.bearFrameworkAddons.formElementsSubmitButton.onClick(this,' . json_encode($waitingText) . ',' . json_encode($waitingClass) . ',' . (int) $enableAfterSubmit . ');';
 echo '<span ' . Utilities::getElementAttributes($attributes) . ' onclick="' . htmlentities($onClick) . '" role="button" tabindex="0" onkeydown="if(event.keyCode===13){this.click();}">' . htmlspecialchars($text) . '</span>';
 echo '</div>';
 echo '</body></html>';
