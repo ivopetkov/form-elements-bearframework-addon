a1fc095517c34f5e8b2bf7d0a413d39dd9550f1a
Ivo Petkov <ivo@ivopetkov.com>
2026-02-28 21:09:02 +0200

    Add Romanian locale.

---
+++ locales/ro.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'ivopetkov.form-element.image.Choose' => 'Alege ...',
+    'ivopetkov.form-element.image.ClearButton' => 'Elimină fișierul selectat',
+    'ivopetkov.form-element.file.Choose' => 'Alege ...',
+    'ivopetkov.form-element.file.ClearButton' => 'Elimină fișierul selectat',
+    'ivopetkov.form-element.file.SelectedFilesCount' => '%s fișiere selectate',
+    'ivopetkov.form-element.file.TooBig' => 'Fișierul selectat este prea mare. Dimensiunea maximă permisă este %s.',
+    'ivopetkov.form-element.image.TooBig' => 'Fișierul selectat este prea mare. Dimensiunea maximă permisă este %s.',
+    'ivopetkov.form-element.datetime.Hours' => 'Ore',
+    'ivopetkov.form-element.datetime.Minutes' => 'Minute',
+    'ivopetkov.form-element.datetime.Seconds' => 'Secunde',
+    'ivopetkov.form-element.rich-textarea.OpenURL' => 'Deschide URL',
+    'ivopetkov.form-element.rich-textarea.Email' => 'Email',
+    'ivopetkov.form-element.rich-textarea.Call' => 'Apelează',
+    'ivopetkov.form-element.rich-textarea.Copy' => 'Copiază',
+    'ivopetkov.form-element.rich-textarea.Share' => 'Distribuie',
+    'ivopetkov.form-element.rich-textarea.Copied' => 'Copiat',
+];
